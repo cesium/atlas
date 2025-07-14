a7bc1db10859fb8299d1b1855e016c6c4c3a25db
RicoPleasure <esprazj.dev@gmail.com>
2025-07-14 20:27:34 +0100

    feat: add plug

--- lib/atlas_web/plugs/authorization_user_types.ex
+++ lib/atlas_web/plugs/authorization_user_types.ex
@@ -1,68 +1,59 @@
-defmodule AtlasWeb.Plugs.AuthorizationUserTypes do
+defmodule AtlasWeb.Plugs.UserRequires do
   import Plug.Conn
   import Phoenix.Controller
+  alias Atlas.Accounts.Guardian
 
   @moduledoc """
   A plug that restricts route access based on user types.
 
   Usage:
     # In your router or controller
-    plug MyAppWeb.Plugs.RequireUserType, user_types: [:admin, :moderator]
+    plug MyAppWeb.Plugs.UserRequires, user_types: [:admin, :moderator]
 
     # Or for a single user type
-    plug MyAppWeb.Plugs.RequireUserType, user_type: :admin
+    plug MyAppWeb.Plugs.UserRequires, user_type: :admin
   """
 
   def init(args) do
     cond do
       user_types = args[:user_types] ->
-        %{user_types: user_types, on_failure: args[:on_failure] || :redirect}
+        %{user_types: user_types}
 
       user_type = args[:user_type] ->
-        %{user_types: [user_type], on_failure: args[:on_failure] || :redirect}
+        %{user_types: [user_type]}
 
       true ->
         raise ArgumentError, "Must provide either :user_type or :user_types option"
     end
   end
 
-  def call(conn, %{user_types: allowed_types, on_failure: on_failure}) do
-    current_user = conn.assigns[:current_user]
+  def call(conn, %{user_types: allowed_types}) do
+    {user, _session} = Guardian.Plug.current_resource(conn)
+    user_type = user.type
 
-    cond do
-      is_nil(current_user) ->
-        handle_unauthorized(conn, on_failure, :not_authenticated)
-
-      is_nil(Map.get(current_user, :user_type)) ->
-        handle_unauthorized(conn, on_failure, :no_user_type)
-
-      current_user.user_type not in allowed_types ->
-        handle_unauthorized(conn, on_failure, :insufficient_permissions)
-
+    case user_type_allowed?(user_type, allowed_types) do
       true ->
         conn
+
+      false ->
+        handle_unauthorized(conn, user, allowed_types)
     end
-  end
 
-  defp handle_unauthorized(conn, :redirect, _reason) do
     conn
-    |> redirect(to: "/")
-    |> halt()
   end
 
-  defp handle_unauthorized(conn, :json, reason) do
-    conn
-    |> json(%{error: unauthorized_message(reason)})
-    |> halt()
+  def user_type_allowed?(user_type, allowed_types) do
+    user_type in allowed_types
   end
 
-  defp handle_unauthorized(conn, :halt, _reason) do
+  def handle_unauthorized(conn, user, allowed_types) do
     conn
     |> put_status(:forbidden)
+    |> json(%{
+      error: "Unauthorized access",
+      user_type: user.type,
+      allowed_types: allowed_types
+    })
     |> halt()
   end
-
-  defp unauthorized_message(:not_authenticated), do: "You must be logged in"
-  defp unauthorized_message(:no_user_type), do: "User type not defined"
-  defp unauthorized_message(:insufficient_permissions), do: "You don't have enough permission"
 end
