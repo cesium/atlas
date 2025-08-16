8458f0a95da2b4a520c3f8144aa4abd8b8f17763
RicoPleasure <esprazj.dev@gmail.com>
2025-08-16 18:55:57 +0100

    chore: add swagger for avatar upload

--- lib/atlas_web/controllers/user_controller.ex
+++ lib/atlas_web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule AtlasWeb.UserController do
   use AtlasWeb, :controller
+  use PhoenixSwagger
 
   alias Atlas.Accounts
 
@@ -14,6 +15,24 @@ defmodule AtlasWeb.UserController do
     conn
     |> put_status(:unprocessable_entity)
     |> json(%{status: "error", message: "No avatar file provided"})
+  end
+
+  swagger_path :upload_avatar do
+    post("/v1/users/{id}/avatar")
+    summary("Upload user avatar")
+    description("Upload an avatar image for a specific user")
+    produces("application/json")
+    tag("Uploaders")
+    consumes("multipart/form-data")
+    security([%{Bearer: []}])
+
+    parameters do
+      id(:path, :string, "User ID", required: true)
+      avatar(:formData, :file, "Avatar image file", required: true)
+    end
+
+    response 200, "Success", Schema.ref(:AvatarUploadSuccess)
+    response 422, "Validation error", Schema.ref(:ErrorResponse)
   end
 
   defp get_user(user_id) do
@@ -47,5 +66,41 @@ defmodule AtlasWeb.UserController do
     end
 
     conn |> put_status(status) |> json(%{status: "error", message: message})
+  end
+
+  def swagger_definitions do
+    %{
+      AvatarUploadSuccess: swagger_schema do
+        title("Avatar Upload Success Response")
+        description("Successful avatar upload response")
+        type(:object)
+        properties do
+          status(:string, "Response status", example: "success")
+          message(:string, "Success message", example: "Avatar uploaded successfully")
+          data(Schema.ref(:AvatarData))
+        end
+        required([:status, :message, :data])
+      end,
+      AvatarData: swagger_schema do
+        title("Avatar Data")
+        description("Avatar upload data")
+        type(:object)
+        properties do
+          avatar_url(:string, "URL of the uploaded avatar", example: "/this/is/an/xXxXxXxX-xxxx-xxxx-xxxx-xxxxxxxxxxxx/example.jpg")
+          user_id(:string, "User UUID", example: "xXxXxXxX-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
+        end
+        required([:avatar_url, :user_id])
+      end,
+      ErrorResponse: swagger_schema do
+        title("Error Response")
+        description("Error response format")
+        type(:object)
+        properties do
+          status(:string, "Response status", example: "error")
+          message(:string, "Error message", example: "User not found")
+        end
+        required([:status, :message])
+      end
+    }
   end
 end
