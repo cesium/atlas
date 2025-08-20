335f2b7acbc8816c552fa38dc983bce47351d738
RicoPleasure <esprazj.dev@gmail.com>
2025-08-20 10:59:58 +0100

    chore: remove all traces of the swagger

--- lib/atlas_web/controllers/job_controller.ex
+++ lib/atlas_web/controllers/job_controller.ex
@@ -1,19 +1,9 @@
 defmodule AtlasWeb.JobController do
   use AtlasWeb, :controller
-  use PhoenixSwagger
 
   alias Atlas.Workers
 
   action_fallback AtlasWeb.FallbackController
-
-  swagger_path :index do
-    get("/v1/jobs")
-    summary("List all jobs")
-    description("Returns a list of all jobs in the system.")
-
-    response(200, "Jobs successfully retrieved", Schema.ref(:JobsResponse))
-    security([%{Bearer: []}])
-  end
 
   def index(conn, _params) do
     {user, _session} = Guardian.Plug.current_resource(conn)
@@ -26,20 +16,6 @@ defmodule AtlasWeb.JobController do
       end
 
     render(conn, :index, jobs: jobs)
-  end
-
-  swagger_path :show do
-    get("/v1/jobs/{id}")
-    summary("Get a job by ID")
-    description("Returns details of a specific job.")
-
-    parameters do
-      id(:path, :string, "ID of the job", required: true)
-    end
-
-    response(200, "Job successfully retrieved", Schema.ref(:JobResponse))
-    response(404, "Job not found")
-    security([%{Bearer: []}])
   end
 
   def show(conn, %{"id" => id}) do
@@ -68,55 +44,5 @@ defmodule AtlasWeb.JobController do
 
   defp user_can_list_all_jobs?(user) do
     user && user.type == :admin
-  end
-
-  def swagger_definitions do
-    %{
-      JobsResponse:
-        swagger_schema do
-          title("JobsResponse")
-          description("Response containing a list of jobs")
-
-          properties do
-            jobs(:array, "List of jobs", items: Schema.ref(:Job))
-          end
-        end,
-      JobResponse:
-        swagger_schema do
-          title("JobResponse")
-          description("Response containing a single job")
-
-          properties do
-            job(Schema.ref(:Job), "Details of the job")
-          end
-        end,
-      Job:
-        swagger_schema do
-          title("Job")
-          description("A job in the system")
-
-          properties do
-            id(:integer, "ID of the job", required: true)
-            type(:string, "Type of the job", required: true)
-            state(:string, "Status of the job", required: true)
-            user_id(:string, "ID of the user who created the job", required: true)
-
-            inserted_at(:string, "Timestamp when the job was created",
-              format: "date-time",
-              required: true
-            )
-
-            attempted_at(:string, "Timestamp when the job was attempted",
-              format: "date-time",
-              required: true
-            )
-
-            completed_at(:string, "Timestamp when the job was completed",
-              format: "date-time",
-              required: true
-            )
-          end
-        end
-    }
   end
 end
