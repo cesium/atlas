4eb1221af9fdce376770fad3a7dc0af221998c5b
João Lobo <joaodlobo@gmail.com>
2025-09-11 15:44:57 +0100

    feat: blackboard data exporters

---
+++ lib/atlas/exporters/blackboard.ex
@@ -0,0 +1,64 @@
+defmodule Atlas.Exporters.Blackboard do
+  @moduledoc """
+  Export data to Blackboard format.
+  """
+  use Atlas.Context
+
+  alias Atlas.University.Degrees.Courses.Shifts.Shift
+  alias Atlas.University.ShiftEnrollment
+
+  def blackboard_course_groups_csv(course_id) do
+    Shift
+    |> where([s], s.course_id == ^course_id)
+    |> where([s], s.type != :theoretical)
+    |> order_by([s], asc: s.type, asc: s.number)
+    |> Repo.all()
+    |> Enum.map(&format_course_group_row/1)
+    |> CSV.encode(separator: ?;)
+  end
+
+  def blackboard_course_group_enrollments_csv(course_id) do
+    ShiftEnrollment
+    |> join(:inner, [se], s in Shift, on: se.shift_id == s.id)
+    |> where([se, s], se.status in [:active, :inactive])
+    |> where([se, s], s.course_id == ^course_id)
+    |> where([se, s], s.type != :theoretical)
+    |> order_by([se, s], asc: s.type, asc: s.number)
+    |> preload([se, s], [:student, :shift])
+    |> Repo.all()
+    |> Enum.map(&format_course_group_enrollment_row/1)
+    |> CSV.encode(separator: ?;, force_escaping: true)
+  end
+
+  defp format_course_group_row(shift) do
+    shift_short_name = Shift.short_name(shift)
+
+    [
+      shift_short_name,
+      shift_short_name,
+      nil,
+      nil,
+      "S",
+      nil,
+      "N",
+      nil,
+      nil,
+      nil,
+      nil,
+      nil
+    ]
+  end
+
+  defp format_course_group_enrollment_row(%ShiftEnrollment{student: student, shift: shift}) do
+    shift_short_name = Shift.short_name(shift)
+    normalized_student_number = String.downcase(student.number)
+
+    [
+      shift_short_name,
+      normalized_student_number,
+      normalized_student_number |> String.replace("a", "") |> String.replace("pg", ""),
+      normalized_student_number,
+      "."
+    ]
+  end
+end
