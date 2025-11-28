2aae38af356196ce95dd3cb18a3155e06b654225
Enrico Prazeres <esprazj.dev@gmail.com>
2025-11-28 01:21:33 +0000

    fix: case when students have category events associated

--- priv/repo/migrations/20251127222831_modify_users_event_categories.exs
+++ priv/repo/migrations/20251127222831_modify_users_event_categories.exs
@@ -7,5 +7,11 @@ defmodule Atlas.Repo.Migrations.ModifyEvents do
              references(:event_categories, on_delete: :delete_all, type: :binary_id),
              from: references(:event_categories, on_delete: :nothing, type: :binary_id)
     end
+
+    alter table(:users_event_categories) do
+      modify :event_category_id,
+            references(:event_categories, on_delete: :delete_all, type: :binary_id),
+            from: references(:event_categories, on_delete: :nothing, type: :binary_id)
+    end
   end
 end
