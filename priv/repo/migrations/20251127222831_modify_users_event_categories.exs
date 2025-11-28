7152411bfa2e55814a50e3f703b3578af3f15337
Enrico Prazeres <esprazj.dev@gmail.com>
2025-11-28 01:22:38 +0000

    fix: format

--- priv/repo/migrations/20251127222831_modify_users_event_categories.exs
+++ priv/repo/migrations/20251127222831_modify_users_event_categories.exs
@@ -10,8 +10,8 @@ defmodule Atlas.Repo.Migrations.ModifyEvents do
 
     alter table(:users_event_categories) do
       modify :event_category_id,
-            references(:event_categories, on_delete: :delete_all, type: :binary_id),
-            from: references(:event_categories, on_delete: :nothing, type: :binary_id)
+             references(:event_categories, on_delete: :delete_all, type: :binary_id),
+             from: references(:event_categories, on_delete: :nothing, type: :binary_id)
     end
   end
 end
